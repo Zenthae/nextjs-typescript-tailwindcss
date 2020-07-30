19bf782f3b60f2a6342f76e7d52b14c0a6910a63
Zenthae <zenthae@gmail.com>
2020-07-30 21:21:46 +0200

    Updates

---
+++ _next/static/O4_v-kwubjFllSE4UmAOB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-0b73bdc418166b62f6c0.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f45a89de560d870302bd.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
